4015b9cde9f69bcad435e8d4728b17f8a5a30914
gofastdata <45173067+gofastdata@users.noreply.github.com>
2018-11-19 09:20:19 -0500

    Add files via upload

---
+++ CEODataClean.sql
@@ -0,0 +1,14 @@
+
+use ceodb
+go
+
+delete from crsCompanyPerson
+delete from crsSubscriptionPublication
+delete from crsPersonAddress
+delete from tblPerson
+delete from tblCompany
+delete from tblSubscriptions
+delete from tblAddress
+delete from loaderrors
+
+
